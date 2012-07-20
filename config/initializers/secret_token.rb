8070fe066bddb4a4b2a1163f4892d1f02d750a80
Patrick Cushing <patrick.j.cushing@gmail.com>
2012-07-19 17:44:48 -0700

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UrlShortener::Application.config.secret_token = 'e617c746efe55c3fec1f2411faa70d8b30bf1cbdf6dcfc434e0a03951b72b16ba21dd340abb86a4a3043101731a03d56ba6976171eed5fa71b14a3dbc5389a95'
